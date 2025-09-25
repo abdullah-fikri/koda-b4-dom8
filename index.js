06296028463bc9c19170b52af1406ee613202a1c
Abdulah-fikri <abdullahfikri981@gmail.com>
2025-09-25 23:10:50 +0700

    use library axios for get data api and jquery for javascript

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
-const input = document.getElementById("input")
-const btn = document.getElementById("btn")
+const $input = $("#input")
+const $btn = $("#btn")
 
 let allData = []
+
 async function main(){
-    const req = await fetch("https://rickandmortyapi.com/api/character")
-    const data = await req.json()
-    allData = data.results
-    showdata(allData)
+    try {
+        const response = await axios.get("https://rickandmortyapi.com/api/character")
+        allData = response.data.results
+        showdata(allData)
+    } catch (error) {
+        console.log("error", error)
+    }
 }
 
 function showdata(data){
-    const main = document.getElementById("main")
-    main.innerHTML = ""
-    data.forEach(element => {
-        const box = document.createElement("div")
-        box.classList.add("card")
-        box.innerHTML += `<img src="${element.image}">
-        <h3>${element.name}</h3>`
-        
-        main.append(box)
-    })
+    const $main = $("#main")
+    $main.html("") 
 
+    data.forEach(element => {
+        const $box = $("<div></div>").addClass("flex border items-center bg-white rounded-[20px] flex-col")
+        const $img = $("<img>").attr("src", element.image).addClass("w-[80%]")
+        const $name = $("<h3></h3>").text(element.name)
+
+        $box.append($img)
+        $box.append($name)
+
+        $main.append($box)
+    })
 }
-btn.addEventListener("click", (e)=>{
+
+$btn.on("click", function(e){
     e.preventDefault()
-    const inputUsr = input.value.toLowerCase()
+    const inputUsr = $input.val().toLowerCase()
     const hasil = allData.filter(item =>
         item.name.toLowerCase().includes(inputUsr)
     )
     showdata(hasil)
 })
-
 
 main()
